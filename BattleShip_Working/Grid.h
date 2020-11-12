4c38119b6beddd2dc1a5222879e140a9bc9dfee0
brucetieu <brucetieu5@gmail.com>
2020-11-12 15:07:30 -0700

    Fix issue with 2d array

--- BattleShip_Working/Grid.h
+++ BattleShip_Working/Grid.h
@@ -2,17 +2,21 @@
 #define GRID_H
 #include <vector>
 
+
 class Grid
 {
 private:
-    static const int MAX_ROW = 10;
-    static const int MAX_COLUMN = 10;
-    char GRID[MAX_ROW][MAX_COLUMN] = {{'0'}};
+//
+     static const int MAX_ROW = 10;
+     static const int MAX_COLUMN = 10;
+//    char GRID[MAX_ROW][MAX_COLUMN] = {{'0'}};
+//     char GRID[MAX_COLUMN][MAX_COLUMN];
     int row, column;
     const std::vector<char> MAP = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
 
 public:
+    char GRID[MAX_ROW][MAX_COLUMN] = {{'0'}};
     Grid();
     Grid(int &row, int &column);
     int getRow() const;
