e7a6c5e12fe70e63488fd218d446cdc95b0d174a
brucetieu <brucetieu5@gmail.com>
2020-11-19 19:41:41 -0700

    Successful compilation

--- BattleShip_Working/main.cpp
+++ BattleShip_Working/main.cpp
@@ -5,19 +5,18 @@
 #include "Ship.h"
 #include "Helpers.h"
 #include "Player.h"
-//#include "Computer.h"
+#include "Computer.h"
 
 using namespace std;
 
 //char Grid::GRID[Grid::MAX_COLUMN][Grid::MAX_COLUMN] = {{'0'}};
 int main()
 {
-//    Computer computer;
-//    computer.generateCorrectPlacements();
-//    computer.printRandShips();
 
-//    Ship ship;
-//    ship.readInShips();
+    Computer computer;
+    computer.generateCorrectPlacements();
+    computer.printRandShips();
+
     Player player;
     Game game;
 
@@ -29,40 +28,7 @@ int main()
 
     Grid grid = player.placeShipsOnBoard(vecOfShips, newVecOfShips);
     grid.printGrid();
-//    player.addSizeToShips();
-//    player.replaceShipLocations();
-//    player.checkAllShipsAreIncluded();
-//    if (player.shipsAreInBounds()) {
-//        cout << "In bounds" << endl;
-//    } else cout << "Out of bounds" << endl;
-//    player.getPossibleShipLocations();
-//    Grid grid = player.placeShipsOnBoard();
-//    grid.printGrid();
-//    if (player.shipsOverlap()) {
-//        cout << "No overlap" << endl;
-//    } else cout << "Overlap" << endl;
-//    player.printShipVector();
-    
-    // Grid grid;
 
-    // grid.createGrid();
-
-    // int i = 0;
-    // while (i < 1)
-    // {
-
-    //     string x;
-    //     cout << "Chose a location: ";
-    //     cin >> x;
-
-    //     Grid grid_ = grid.chooseGridLoc(x);
-    //     grid.updateGrid(grid_);
-
-    //     i++;
-    // }
-
-    // grid.clearGrid();
-    // grid.printGrid();
 
     return 0;
 }
