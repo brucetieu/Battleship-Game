460313c5ce105862d3e925fb51b45520d12fdfe7
brucetieu <brucetieu5@gmail.com>
2020-11-19 17:07:59 -0700

    fix shipsAreInBounds(), add comments

--- BattleShip_Working/Game.cpp
+++ BattleShip_Working/Game.cpp
@@ -16,10 +16,13 @@ using namespace std;
  * @param vecOfShips
  * @return
  */
-bool Game::shipsAreInBounds(vector <Ship> &vecOfShips) {
-    for (int i = 0; i < vecOfShips.size(); i++) {
-        for (int j = 0; j < vecOfShips[i].possibleShipLocations.size(); j++) {
-            if (vecOfShips[i].possibleShipLocations[j].length() > 2) return false;
+bool Game::shipsAreInBounds(vector <Ship> &newVecOfShips) {
+    for (int i = 0; i < newVecOfShips.size(); i++) {
+        for (int j = 0; j < newVecOfShips[i].possibleShipLocations.size(); j++) {
+            if (newVecOfShips[i].possibleShipLocations[j].length() > 2) {
+                cout << "Ship " << newVecOfShips[i].shipType << " is out of bounds: " << newVecOfShips[i].possibleShipLocations[j] << endl;
+                return false;
+            }
         }
     }
     return true;
@@ -32,14 +35,14 @@ bool Game::shipsAreInBounds(vector <Ship> &vecOfShips) {
  */
 bool Game::allShipsAreIncluded(std::vector <Ship> &vecOfShips) {
     if (vecOfShips.size() < 5) {
-        cout << "Not all ships are included." << endl;
+        cout << "Num of ships: " << vecOfShips.size() << " Not all ships are included." << endl;
         return false;
     }
     return true;
 }
 
 /**
- *
+ * Get all possible locations for each ship. For example,
  * @param vecOfShips The original vector of ships which was populated from ships in the file.
  * @return
  */
@@ -49,16 +52,19 @@ vector<Ship> Game::getPossibleShipLocations(std::vector <Ship> &vecOfShips) {
 
     for (int i = 0; i < vecOfShips.size(); i++) {
         vector<string> tempVector;
+
+        // Parse location of A1 -> 00, A2 -> 01, etc.
         GridIndex indices = helpers.parseShipLocation(vecOfShips[i].shipLocation);
 
         // Fix rows, update columns.
         if (vecOfShips[i].shipOrientation.find("H") != std::string::npos) {
             for (int row = indices.row; row <= indices.row; row++) {
                 for (int col = indices.column; col < vecOfShips[i].shipSize + indices.column; col++) {
+                    // Get row, col representation of each ship location.
                     string x = to_string(row);
                     string y = to_string(col);
                     string result = x + y;
-                    tempVector.push_back(result);
+                    tempVector.push_back(result); // This holds all the possible locs for each ship.
                 }
                 newVecOfShips.push_back(Ship(vecOfShips[i].shipType, vecOfShips[i].shipSize, tempVector));
             }
@@ -86,6 +92,8 @@ vector<Ship> Game::getPossibleShipLocations(std::vector <Ship> &vecOfShips) {
  * @return
  */
 bool Game::shipsDontOverlap(vector<Ship> &newVecOfShips) {
+
+    // Temp vector to hold unique locations.
     vector<string> uniques;
 
     for (int i = 0; i < newVecOfShips.size(); i++) {
@@ -93,8 +101,10 @@ bool Game::shipsDontOverlap(vector<Ship> &newVecOfShips) {
             if (find(uniques.begin(), uniques.end(), newVecOfShips[i].possibleShipLocations[j]) == uniques.end()) {
                 uniques.push_back(newVecOfShips[i].possibleShipLocations[j]);
             }
+
+            // If we see an element twice => overlap!
             else {
-                cout << "Ships overlap" << endl;
+                cout << "Overlap in this location: " << newVecOfShips[i].possibleShipLocations[j] << endl;
                 return false;
             }
         }
