751c9488a5465d8691ea1556942d9f40f9d1dc7b
brucetieu <brucetieu5@gmail.com>
2020-11-30 13:16:06 -0700

    Remove comments

--- BattleShip_Working/main.cpp
+++ BattleShip_Working/main.cpp
@@ -14,45 +14,7 @@ int main()
 {
     Game game;
     Game newGame = game.buildGame("ship_placement1_good.csv");
-
-
     game.playGame(newGame);
-//    Human human;
-
-//    human.fire(newGame);
-//    Game game;
-//    game.playGame();
-
-    // Computer
-//    Computer computer;
-//    vector<Ship> computerPossibleShipLocs = computer.generateCorrectPlacements();
-//    Grid computerBoard = computer.placeShipsOnBoard();
-
-//    grid1.printGrid();
-//    computer.printRandShips();
-
-    // Human.
-//    Human human;
-//
-//    vector<Ship> vecOfShips;
-//    vector<Ship> newVecOfShips;
-//
-//    vecOfShips = human.readShipsFromFile("ship_placement1_good.csv");
-//    newVecOfShips = human.getPossibleShipLocations(vecOfShips);
-//
-//    Grid humanBoard = human.placeShipsOnBoard(vecOfShips, newVecOfShips);
-//    cout << "Here is your ship configuration: " << endl;
-//    humanBoard.printGrid();
-//
-//    cout << endl;
-
-//    cout << "Here is your tracking grid of shots you have fired: " << endl;
-//    Grid trackerBoard;
-//    trackerBoard.createGrid();
-//    trackerBoard.printGrid();
-//
-//    human.fire();
-
 
     return 0;
 }
