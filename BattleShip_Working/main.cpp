ca4b5be2043470fe3811ee4f96dd52c8414d6d7d
brucetieu <brucetieu5@gmail.com>
2020-11-30 13:33:34 -0700

    Added comments

--- BattleShip_Working/main.cpp
+++ BattleShip_Working/main.cpp
@@ -9,8 +9,13 @@
 
 using namespace std;
 
+/**
+ * Main driver for playing the game.
+ * @return 0 to indicate a successful program.
+ */
 int main()
 {
+    // Create a
     Game game;
     Game newGame = game.buildGame("ship_placement1_good.csv");
     game.playGame(newGame);
