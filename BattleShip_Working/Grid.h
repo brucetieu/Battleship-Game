d2e4bd2a1653a104e5e09bfc90d181306161894a
brucetieu <brucetieu5@gmail.com>
2020-12-03 15:38:27 -0700

    Add Comments

--- BattleShip_Working/Grid.h
+++ BattleShip_Working/Grid.h
@@ -9,15 +9,15 @@
 class Grid {
 private:
 
-    static const int MAX_ROW = 10;
+    static const int MAX_ROW = 10;  // Board is 10 x 10
     static const int MAX_COLUMN = 10;
     int row, column;
     std::vector<char> MAP = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                             'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+                             'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};  // List of alphabets to map a character to an index.
 
 
 public:
-    char GRID[MAX_ROW][MAX_COLUMN] = {{'0'}};
+    char GRID[MAX_ROW][MAX_COLUMN] = {{'0'}};  // Initialize empty 2D array.
 
     Grid();
 
@@ -33,7 +33,7 @@ public:
 
     std::vector<char> getMAP() const;
 
-    friend std::ostream &operator<<( std::ostream &output, const Grid &grid );
+    friend std::ostream &operator<<( std::ostream &output, const Grid &grid );  // Overload ostream >> operator.
 
 };
 
