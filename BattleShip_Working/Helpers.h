ebfd3fa9fbfc3e524c539b558a0a70cc4b78e0e5
brucetieu <brucetieu5@gmail.com>
2020-12-03 14:56:27 -0700

    Add more comments

--- BattleShip_Working/Helpers.h
+++ BattleShip_Working/Helpers.h
@@ -14,19 +14,19 @@
 class Helpers {
 
 private:
-    Grid grid;
+    Grid grid;  // Use Grid class throughout class.
 public:
 
-    GridIndex parseLocationToIndex(std::string &location);
-    std::string parseLocationToString(std::string &location);
+    GridIndex parseLocationToIndex(std::string &location);  // Parse a location to row and col.
+    std::string parseLocationToString(std::string &location); // Parse location to row and col string representation.
 
-    bool isAlphabet(std::string &choice);
-    bool isInBound(std::string &choice);
-    bool isRightLength(std::string &choice);
-    bool isSpace(std::string &choice);
-    bool isEmpty(std::string &choice);
+    bool isAlphabet(std::string &choice);  // Check input is alphabetical.
+    bool isInBound(std::string &choice);  // Check location is in bound.
+    bool isRightLength(std::string &choice);  // Check string is within a certain correct range.
+    bool isSpace(std::string &choice); // Check there are no spaces in the input.
+    bool isEmpty(std::string &choice);  // Check that the input is not empty.
 
-    std::string toUpper(std::string &str);
+    std::string toUpper(std::string &str);  // Convert input to all uppercase.
 
 };
 
--- BattleShip_Working/main.cpp
+++ BattleShip_Working/main.cpp
@@ -17,8 +17,10 @@ int main()
 {
     // Create a game instance and play it.
     Game game;
+
+    // Specify the ship arrangement file you want to pass in.
     Game newGame = game.buildGame("ship_placement1_good.csv");
-    game.playGame(newGame);
+    game.playGame(newGame);  // Play the game.
 
     return 0;
 }
